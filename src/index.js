f287abf89441ec4d64734d3b7f3455ee6ff8d92e
Peter <peteryurich@gmail.com>
2022-08-08 00:48:19 +0300

    ending

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import _ from 'lodash';
+import {fetchCountries} from "./fetchCountries";
+import "./fetchCountries"
+
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box')
+
+inputEl.addEventListener('input', _.debounce(fetchCountries, 300))
+
+// inputEl.addEventListener('input', _.debounce((e) => {
+//     console.log(e.target.value)
+// }, 300))
+
