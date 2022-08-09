b4b79467fa6e18d94d8496a41d3702d8e27b74a0
Peter <peteryurich@gmail.com>
2022-08-09 14:01:47 +0300

    com

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,59 +1,9 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-import Notiflix from "notiflix";
-
-const countryInfo = document.querySelector('.country-info')
-
-const fetchCountries = (e) => {
-    e.preventDefault();
-    let inputValue = e.target.value.trim();
-    if (inputValue === "") {
-        countryInfo.innerHTML = '';
-        return
-    }
-
-    fetch(`https://restcountries.com/v3.1/name/${inputValue}?fields=name,capital,population,flags,languages`)
-        .then((response) =>
-        response.json()
-        )
-        .then((array) => {
-            if (array.length >= 10) {
-                Notiflix.Notify.failure('too much countries')
-                console.log(array)
-            }
-            return array
-        })
-        .then((severalCountriesArray) => {
-            if (severalCountriesArray.length > 1 && severalCountriesArray.length < 11) {
-                console.log("there are 2...10 countries. I need to make a markup")
-                countryInfo.innerHTML = severalCountriesArray.reduce((acc, country) => {
-                    return acc + `
-                    <img class="image"src="${country.flags.svg}" >
-                    <h2>${country.name.official}</h2>`
-                }, "")
-                return
-            }
-            return severalCountriesArray
-        })
-        .then((oneCountryArray) => {
-            // if (oneCountryArray.length === 1) {
-                console.log("it's found one country")
-                countryInfo.innerHTML = oneCountryArray.reduce((acc, country) => {
-                    return acc + `
-                    <img class="image"src="${country.flags.svg}" />
-                    <h2>${country.name.official}</h2>
-                    <p>capital: ${country.capital}</p>
-                    <p>population: ${country.population}</p>
-                    <p>languages: ${Object.values(country.languages)}</p>`
-                }, "")
-                // }
-                return oneCountryArray
-        })
-        .catch(() => {
-            Notiflix.Notify.failure('"Oops, there is no country with that name"')
-            countryInfo.innerHTML = '';
-        })
-};
-
+const fetchCountries = (inputValue) => {
+    const url = `${BASE_URL}${inputValue}?fields=name,capital,population,flags,languages`
+    return fetch(url).then(response => response.json())
+}
 
 export { fetchCountries };
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,76 @@
 import './css/styles.css';
 import _ from 'lodash';
 import {fetchCountries} from "./fetchCountries";
-import "./fetchCountries"
+// import "./fetchCountries"
+import Notiflix from "notiflix";
+
 
 
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box')
+const countryInfo = document.querySelector('.country-info')
 
-inputEl.addEventListener('input', _.debounce(fetchCountries, 300))
 
-// inputEl.addEventListener('input', _.debounce((e) => {
-//     console.log(e.target.value)
-// }, 300))
+inputEl.addEventListener('input', _.debounce(onSearch, 300))
 
+function onSearch (e) {
+    e.preventDefault();
+    const inputValue = e.target.value.trim();
+
+    if (inputValue === "") {
+        countryInfo.innerHTML = '';
+        console.log('the value is empty')
+        return
+    }
+
+    fetchCountries(inputValue)
+    .then(alertIfToMany)
+    .then(renderSeveralCountries)
+    .then(renderOneCountry)
+    .catch(onError)
+}
+
+function alertIfToMany (array) {
+    if (array.length > 10) {
+        countryInfo.innerHTML = '';
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+        console.log("too many")
+        return
+    }
+    return array
+}
+
+function renderSeveralCountries (severalCountriesArray) {
+    if (severalCountriesArray.length > 1 && severalCountriesArray.length < 11) {
+        console.log("there are 2...10 countries. I need to make a markup")
+        countryInfo.innerHTML = severalCountriesArray.reduce((acc, country) => {
+            return acc + `
+            <img class="image"src="${country.flags.svg}" >
+            <h2>${country.name.official}</h2>`
+        }, "")
+        return
+    }
+    return severalCountriesArray
+}
+
+function renderOneCountry (oneCountryArray) {
+    // if (oneCountryArray.length === 1) {
+        console.log("it's found one country")
+        countryInfo.innerHTML = oneCountryArray.reduce((acc, country) => {
+            return acc + `<img class="image"src="${country.flags.svg}" />
+            <h2>${country.name.official}</h2>
+            <p>capital: ${country.capital}</p>
+            <p>population: ${country.population}</p>
+            <p>languages: ${Object.values(country.languages)}</p>`
+        }, "")
+        // }
+        return 
+}
+
+function onError ()  {
+    // if (response.status = "404") {
+        Notiflix.Notify.failure("Oops, there is no country with that name")
+        countryInfo.innerHTML = '';
+    // }
+}
